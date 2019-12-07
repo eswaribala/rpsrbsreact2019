c1a923cc0e56749cc78b9a128f38c4dcaf1b9782
eswaribala <Parameswaribala@gmail.com>
2019-12-07 17:10:30 +0530

    Add files via upload

---
+++ messageenc.js
@@ -0,0 +1,29 @@
+// Nodejs encryption with CTR
+var stringify = require('json-stringify');
+var crypto = require('crypto'),
+    algorithm = 'aes-256-ctr',
+    password = 'd6F3Efeq';
+
+function encrypt(text){
+    var cipher = crypto.createCipher(algorithm,password)
+    var crypted = cipher.update(text,'utf8','hex')
+    crypted += cipher.final('hex');
+    return crypted;
+}
+
+function decrypt(text){
+    var decipher = crypto.createDecipher(algorithm,password)
+    var dec = decipher.update(text,'hex','utf8')
+    dec += decipher.final('utf8');
+    return dec;
+}
+
+obj={
+    id:357698,
+    name:"Anoop"
+}
+
+var encryptedText = encrypt(stringify(obj));
+console.log(encryptedText);
+
+console.log(decrypt(encryptedText));
